78a21bbe655954de065b04f87565bca3d0b75bd3
Chris Watts <chris@cjwatts.com>
2020-12-17 18:28:32 +0000

    Revert "Fix issue with -isystem when compiling swift code. (#72)"
    This reverts commit e7a37bebf84e3997322b6f14d6e08bd54b683371.

--- libs/header.cmake
+++ libs/header.cmake
@@ -1,7 +1,12 @@
 # Define the header-only Boost target
 add_library(Boost::boost INTERFACE IMPORTED GLOBAL)
-
-target_include_directories(Boost::boost SYSTEM INTERFACE ${BOOST_SOURCE})
+if(CMAKE_GENERATOR MATCHES "Xcode")
+  # The CMake Xcode generator doesn't support system headers directly
+  set_target_properties(Boost::boost PROPERTIES INTERFACE_COMPILE_OPTIONS "-isystem;${BOOST_SOURCE}"    )
+else()
+  set_target_properties(Boost::boost PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${BOOST_SOURCE}    )
+  set_target_properties(Boost::boost PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${BOOST_SOURCE})
+endif()
 
 # Disable autolink
-target_compile_definitions(Boost::boost INTERFACE BOOST_ALL_NO_LIB=1)
+set_property(TARGET Boost::boost APPEND PROPERTY INTERFACE_COMPILE_DEFINITIONS BOOST_ALL_NO_LIB=1)
